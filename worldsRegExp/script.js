8acbfc57d66c955ef07daffbd1b0ced2add2af1a
Alexander Priyomko <sashapriyomko@mail.ru>
2020-11-14 02:16:25 +0600

    Create script.js

---
+++ worldsRegExp/script.js
@@ -0,0 +1,21 @@
+var regex = '\\b(\\w|[-'])+\\b';
+
+function doFunction()
+{	
+	regex = new RegExp(document.getElementById('regexp').value, 'gm');
+	console.log(regex);
+	const str = document.getElementById('input_data_color_gray').value;
+	let m;
+	var output = '';
+	while ((m = regex.exec(str)) !== null) {
+		if (m.index === regex.lastIndex) {
+			regex.lastIndex++;
+		}		
+		output += `${m[0]}\n`;
+	}
+
+	document.getElementById('output_data_color_gray').value = output;
+}
+
+document.getElementById("clickMe").onclick = doFunction;
+document.getElementById("regexp").value = regex;
