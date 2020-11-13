e3f6c55a6051f5e07ae25ba03422900eeeef5d27
Alexander Priyomko <sashapriyomko@mail.ru>
2020-11-14 02:04:42 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,21 @@
-const regex = /\#([4-9A-C][0-9A-F])\1{2}|[Rr][Gg][Bb]\(((([6-9][0-9]),\4,\4)|(([1][0-9][0-9]),\6,\6)|(([2][0][0-7]),\8,\8))\)/gm;
-const str = `#716064
-#101010
-#606160
-#909190
-#9090 90
-#90 9090
-#AAAA AA
-#ABBBAB
-#B0B0B1
-RGB(100,100,100,)
-RGB(209,209,209)
-RGB(140,150,160,)
-RGB(140,150,160)
-RGB(0,0,0)
-RGB(59,59,59)
-RGB(099,099,099)
-#606060
-#505050
-#909090
-#C0C0C0
-#CFCFCF
-#ABABAB
-#B0B0B0
-RGB(60,60,60)
-RGB(100,100,100)
-RGB(200,200,200)
-RGB(207,207,207)
-RGB(99,99,99)
-`;
-let m;
+var regex = '\\#([4-9A-C][0-9A-F])\\1{2}|[Rr][Gg][Bb]\\(((([6-9][0-9]),\\4,\\4)|(([1][0-9][0-9]),\\6,\\6)|(([2][0][0-7]),\\8,\\8))\\)';
 
-while ((m = regex.exec(str)) !== null) {
-    // This is necessary to avoid infinite loops with zero-width matches
-    if (m.index === regex.lastIndex) {
-        regex.lastIndex++;
-    }
-    
-    // The result can be accessed through the `m`-variable.
-    m.forEach((match) => {
-        console.log(`Found match: ${match}`);
-    });
+function doFunction()
+{	
+	regex = new RegExp(document.getElementById('regexp').value, 'gm');
+	console.log(regex);
+	const str = document.getElementById('input_data_color_gray').value;
+	let m;
+	var output = '';
+	while ((m = regex.exec(str)) !== null) {
+		if (m.index === regex.lastIndex) {
+			regex.lastIndex++;
+		}		
+		output += `${m[0]}\n`;
+	}
+
+	document.getElementById('output_data_color_gray').value = output;
 }
+
+document.getElementById("clickMe").onclick = doFunction;
+document.getElementById("regexp").value = regex;
