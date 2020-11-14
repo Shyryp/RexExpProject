c914c1ae9d4b6d76744b044451fd15a2fda86cb4
Alexander Priyomko <sashapriyomko@mail.ru>
2020-11-14 12:24:04 +0600

    Update script.js

--- colorsRexExp/script.js
+++ colorsRexExp/script.js
@@ -2,20 +2,16 @@ var regex = '\\#([4-9A-C][0-9A-F])\\1{2}|rgb\\(((([6-9][0-9]),\\4,\\4)|((1[0-9][
 
 function doFunction()
 {		
-	regex = new RegExp(document.getElementById('regexp').value, 'gmi');
+	regex = new RegExp(document.getElementById('regexp').value, 'mi');
 	console.log(regex);
 	const str = document.getElementById('input_data_color_gray').value;
 	var data = str.split("\n");
 	let m;
 	var output = '';
-	//while ((m = regex.exec(str)) !== null) {
 	for(var color of data)
 	{
 		if((m = regex.exec(color)) !== null)
-		{
-			if (m.index === regex.lastIndex) {
-				regex.lastIndex++;
-			}		
+		{	
 			output += `Входное значение: ${m[0]}\nНайдено совпадение: ${m[0]}\nРезультат теста: Положительный\n\n`;
 		}
 		else
@@ -23,7 +19,6 @@ function doFunction()
 			output += `Входное значение: ${color}\nРезультат теста: Отрицательный\n\n`;
 		}
 	}
-	//}
 
 	document.getElementById('output_data_color_gray').value = output;
 }
